9478ee5648328d8510f9021303075c506a25d179
bennybenbensisko <bennybenbensisko@gmail.com>
2024-08-19 21:26:29 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.google.cloud.tools.jib") version "3.3.1";
     id("org.springframework.boot") version "3.3.2";
     id("jacoco");
-    id("org.sonarqube") version "3.5.0.2730";
+    id("org.sonarqube") version "3.3";
     id("java");
     id("eclipse");
     id("idea");
