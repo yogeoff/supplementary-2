b67081af2bff35261db81695806837599693a600
L00179654 <L00179654@users.noreply.github.com>
2024-08-22 20:18:04 +0100

    Updated formatting

--- src/main/java/org/sasanlabs/configuration/VulnerableAppConfiguration.java
+++ src/main/java/org/sasanlabs/configuration/VulnerableAppConfiguration.java
@@ -40,130 +40,130 @@ import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 @Configuration
 public class VulnerableAppConfiguration {
 
-    private static final String I18N_MESSAGE_FILE_LOCATION = "classpath:i18n/messages";
-    private static final String ATTACK_VECTOR_PAYLOAD_PROPERTY_FILES_LOCATION_PATTERN =
-            "classpath:/attackvectors/*.properties";
-    private static final List<String> MAX_FILE_UPLOAD_SIZE_OVERRIDE_PATHS =
-            Arrays.asList(
-                    "/" + UnrestrictedFileUpload.CONTROLLER_PATH + "/" + LevelConstants.LEVEL_9);
+  private static final String I18N_MESSAGE_FILE_LOCATION = "classpath:i18n/messages";
+  private static final String ATTACK_VECTOR_PAYLOAD_PROPERTY_FILES_LOCATION_PATTERN =
+          "classpath:/attackvectors/*.properties";
+  private static final List<String> MAX_FILE_UPLOAD_SIZE_OVERRIDE_PATHS =
+          Arrays.asList(
+                  "/" + UnrestrictedFileUpload.CONTROLLER_PATH + "/" + LevelConstants.LEVEL_9);
 
-    /**
-     * Will Inject MessageBundle into messageSource bean.
-     *
-     * @return resourceBundle
-     */
-    @Bean
-    public ReloadableResourceBundleMessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource =
-                new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename(I18N_MESSAGE_FILE_LOCATION);
-        messageSource.setCacheSeconds(100);
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
+  /**
+   * Will Inject MessageBundle into messageSource bean.
+   *
+   * @return resourceBundle
+   */
+  @Bean
+  public ReloadableResourceBundleMessageSource messageSource() {
+    ReloadableResourceBundleMessageSource messageSource =
+            new ReloadableResourceBundleMessageSource();
+    messageSource.setBasename(I18N_MESSAGE_FILE_LOCATION);
+    messageSource.setCacheSeconds(100);
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
+  }
+
+  @Bean
+  public AcceptHeaderLocaleResolver localeResolver() {
+    final AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
+    resolver.setDefaultLocale(Locale.US);
+    return resolver;
+  }
+
+  /**
+   * This method reads all the property which are useful for vulnerableApp and then injects them
+   * into the context so that entire application can use it.
+   *
+   * @param resourceLoader
+   * @return {@link VulnerableAppProperties} which is injected in spring context.
+   * @throws IOException
+   */
+  @Bean
+  public VulnerableAppProperties propertyLoader(ResourceLoader resourceLoader)
+          throws IOException {
+    Resource[] attackVectorsResources =
+            new PathMatchingResourcePatternResolver()
+                    .getResources(ATTACK_VECTOR_PAYLOAD_PROPERTY_FILES_LOCATION_PATTERN);
+    Properties attackVectorProperties = new Properties();
+    for (Resource attackVectorResource : attackVectorsResources) {
+      PropertiesLoaderUtils.fillProperties(attackVectorProperties, attackVectorResource);
     }
+    VulnerableAppProperties vulnerableAppProperties =
+            new VulnerableAppProperties(attackVectorProperties);
+    return vulnerableAppProperties;
+  }
 
-    @Bean
-    public AcceptHeaderLocaleResolver localeResolver() {
-        final AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
-        resolver.setDefaultLocale(Locale.US);
-        return resolver;
-    }
+  /**
+   * DB Configuration Below configuration is done to restrict the Application user rights and not
+   * to give admin access rights to it. This is quite important because in case of any
+   * Vulnerability in application it reduces the impact of the destruction because of the
+   * vulnerability.
+   */
+  @Bean
+  @Primary
+  @ConfigurationProperties("spring.datasource.admin")
+  public DataSourceProperties adminDataSourceProperties() {
+    return new DataSourceProperties();
+  }
 
-    /**
-     * This method reads all the property which are useful for vulnerableApp and then injects them
-     * into the context so that entire application can use it.
-     *
-     * @param resourceLoader
-     * @return {@link VulnerableAppProperties} which is injected in spring context.
-     * @throws IOException
-     */
-    @Bean
-    public VulnerableAppProperties propertyLoader(ResourceLoader resourceLoader)
-            throws IOException {
-        Resource[] attackVectorsResources =
-                new PathMatchingResourcePatternResolver()
-                        .getResources(ATTACK_VECTOR_PAYLOAD_PROPERTY_FILES_LOCATION_PATTERN);
-        Properties attackVectorProperties = new Properties();
-        for (Resource attackVectorResource : attackVectorsResources) {
-            PropertiesLoaderUtils.fillProperties(attackVectorProperties, attackVectorResource);
-        }
-        VulnerableAppProperties vulnerableAppProperties =
-                new VulnerableAppProperties(attackVectorProperties);
-        return vulnerableAppProperties;
-    }
+  @Bean
+  @Primary
+  @ConfigurationProperties("spring.datasource.admin.configuration")
+  public DataSource adminDataSource(
+          @Qualifier("adminDataSourceProperties")
+          DataSourceProperties adminDataSourceProperties) {
+    return adminDataSourceProperties
+            .initializeDataSourceBuilder()
+            .type(HikariDataSource.class)
+            .build();
+  }
 
-    /**
-     * DB Configuration Below configuration is done to restrict the Application user rights and not
-     * to give admin access rights to it. This is quite important because in case of any
-     * Vulnerability in application it reduces the impact of the destruction because of the
-     * vulnerability.
-     */
-    @Bean
-    @Primary
-    @ConfigurationProperties("spring.datasource.admin")
-    public DataSourceProperties adminDataSourceProperties() {
-        return new DataSourceProperties();
-    }
+  @Bean
+  @ConfigurationProperties("spring.datasource.application")
+  public DataSourceProperties applicationDataSourceProperties() {
+    return new DataSourceProperties();
+  }
 
-    @Bean
-    @Primary
-    @ConfigurationProperties("spring.datasource.admin.configuration")
-    public DataSource adminDataSource(
-            @Qualifier("adminDataSourceProperties")
-            DataSourceProperties adminDataSourceProperties) {
-        return adminDataSourceProperties
-                .initializeDataSourceBuilder()
-                .type(HikariDataSource.class)
-                .build();
-    }
+  @Bean
+  @ConfigurationProperties("spring.datasource.application.configuration")
+  public DataSource applicationDataSource(
+          @Qualifier("applicationDataSourceProperties")
+          DataSourceProperties applicationDataSourceProperties) {
+    return applicationDataSourceProperties
+            .initializeDataSourceBuilder()
+            .type(HikariDataSource.class)
+            .build();
+  }
 
-    @Bean
-    @ConfigurationProperties("spring.datasource.application")
-    public DataSourceProperties applicationDataSourceProperties() {
-        return new DataSourceProperties();
-    }
+  @Bean
+  public JdbcTemplate applicationJdbcTemplate(
+          @Qualifier("applicationDataSource") DataSource applicationDataSource) {
+    return new JdbcTemplate(applicationDataSource);
+  }
 
-    @Bean
-    @ConfigurationProperties("spring.datasource.application.configuration")
-    public DataSource applicationDataSource(
-            @Qualifier("applicationDataSourceProperties")
-            DataSourceProperties applicationDataSourceProperties) {
-        return applicationDataSourceProperties
-                .initializeDataSourceBuilder()
-                .type(HikariDataSource.class)
-                .build();
-    }
-
-    @Bean
-    public JdbcTemplate applicationJdbcTemplate(
-            @Qualifier("applicationDataSource") DataSource applicationDataSource) {
-        return new JdbcTemplate(applicationDataSource);
-    }
-
-    /**
-     * Customized MultipartFilter bean disables default max upload size for multipart files and
-     * their overall requests, for select paths. See {@link
-     * UnrestrictedFileUpload#getVulnerablePayloadLevel10()} for usage.
-     */
-    @Bean
-    @Order(0)
-    public MultipartFilter multipartFilter() {
-        class MaxUploadSizeOverrideMultipartFilter extends MultipartFilter {
-            @Override
-            protected MultipartResolver lookupMultipartResolver(HttpServletRequest request) {
-                if (MAX_FILE_UPLOAD_SIZE_OVERRIDE_PATHS.contains(request.getServletPath())) {
+  /**
+   * Customized MultipartFilter bean disables default max upload size for multipart files and
+   * their overall requests, for select paths. See {@link
+   * UnrestrictedFileUpload#getVulnerablePayloadLevel10()} for usage.
+   */
+  @Bean
+  @Order(0)
+  public MultipartFilter multipartFilter() {
+    class MaxUploadSizeOverrideMultipartFilter extends MultipartFilter {
+      @Override
+      protected MultipartResolver lookupMultipartResolver(HttpServletRequest request) {
+        if (MAX_FILE_UPLOAD_SIZE_OVERRIDE_PATHS.contains(request.getServletPath())) {
 //                    CommonsMultipartResolver multipart = new CommonsMultipartResolver();
-                    StandardServletMultipartResolver multipart = new StandardServletMultipartResolver();
+          StandardServletMultipartResolver multipart = new StandardServletMultipartResolver();
 //                    multipart.setMaxUploadSize(-1);
 //                    multipart.setMaxUploadSizePerFile(-1);
-                    return multipart;
-                } else {
-                    // returns default implementation
-                    return lookupMultipartResolver();
-                }
-            }
+          return multipart;
+        } else {
+          // returns default implementation
+          return lookupMultipartResolver();
         }
-        ;
-        return new MaxUploadSizeOverrideMultipartFilter();
+      }
     }
+    ;
+    return new MaxUploadSizeOverrideMultipartFilter();
+  }
 }
